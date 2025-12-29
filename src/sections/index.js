aed40a4649b98deb6a1c776a48a71547df001e42
kritik poudel <kritikpoudel808@gmail.com>
2025-12-29 21:26:03 +0545

    initial commit

---
+++ src/sections/index.js
@@ -0,0 +1,21 @@
+import Hero from "./Hero";
+import Nav from "./Nav";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+
+export{
+    Hero,
+    Nav,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    CustomerReviews,
+    Subscribe,
+    Footer,
+}
