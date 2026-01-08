b498f9d27bcedfa307c1e92fd50e14c443029ccc
kritik poudel <kritikpoudel808@gmail.com>
2026-01-08 22:12:11 +0545

    edit name

--- src/constants/index.js
+++ src/constants/index.js
@@ -73,7 +73,7 @@ export const services = [
 export const reviews = [
     {
         imgURL: customer1,
-        customerName: 'Morich Brown',
+        customerName: 'Kritik Poudel',
         rating: 4.5,
         feedback: "The attention to detail and the quality of the product exceeded my expectations. Highly recommended!"
     },
