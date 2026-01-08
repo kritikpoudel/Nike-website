75e4a1b2076a861faa2206a6185fe8716d3c6950
kritik poudel <kritikpoudel808@gmail.com>
2026-01-08 16:01:11 +0545

    implement specialoffer section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
-const Button = ({label,iconUrl}) => {
+const Button = ({label,iconUrl ,backgroundColor,borderColor,textColor}) => {
   return (
-    <button className='flex justify-center items-center bg-coral-red rounded-full text-white px-7 py-4 gap-2 font-montserrat text-lg leading-none border cursor-pointer'>
+    <button className={`flex justify-center items-center border rounded-full px-7 py-4 gap-2 font-montserrat text-lg leading-none 
+    ${backgroundColor? `${borderColor} ${backgroundColor} ${textColor}`:'bg-coral-red text-white border-[#FF6452]'}
+    cursor-pointer`}>
         {label}
        {iconUrl && <img src={iconUrl} alt="icon right" className='rounded-full ml-2 w-5 h-5' />}
     </button>
--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import Button from "../components/Button";
+import { arrowRight } from "../assets/icons";
+import { offer } from "../assets/images";
 
 const SpecialOffer = () => {
   return (
-    <div>SpecialOffer</div>
-  )
-}
+    <section className="flex justify-center items-center max-lg:flex-col-reverse gap-10 max-container">
+      <div className="flex-1">
+        <img
+          src={offer}
+          alt=""
+          height={773}
+          width={687}
+          className="object-contain w-full"
+        />
+      </div>
+      <div className="lex-1 flex flex-col">
+        <h2 className="font-palanquin text-4xl font-bold lg:max-w-lg">
+          <span className="text-[#FF6452]">Special</span> Offer
+        </h2>
+        <p className="info-text lg:max-w-lg mt-4">
+          Enjoy exclusive deals on our premium shoe collections for a limited
+          time. Get top-quality footwear at prices you don’t want to miss.
+        </p>
+        <p className="info-text lg:max-w-lg mt-6">
+          Upgrade your style and comfort today with special discounts available
+          only for a short period. Shop now before the offer ends.
+        </p>
+        <div className="flex mt-11 flex-wrap gap-4">
+          <Button label="Shop Now" iconUrl={arrowRight} />
+          <Button
+            label="Learn more"
+            backgroundColor="bg-white"
+            borderColor="border-[#6D6D6D]"
+            textColor="text-slate-gray"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default SpecialOffer
+export default SpecialOffer;
